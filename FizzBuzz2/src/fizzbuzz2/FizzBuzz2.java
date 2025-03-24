d769655fe83a31238a9f385e310822f681fcfcf1
ArthurSilCosta <147404357+ArthurSilCosta@users.noreply.github.com>
2025-03-24 12:51:19 +0000

    code done

--- FizzBuzz2/src/fizzbuzz2/FizzBuzz2.java
+++ FizzBuzz2/src/fizzbuzz2/FizzBuzz2.java
@@ -14,14 +14,18 @@ public class FizzBuzz2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
-        FizzBuzz
-        Print the numbers between 1 and 150
-        If a number is divisible by 3, print Fizz
-        If a number is divisible by 5, print Buzz
-        If a number is divisible by both, print FizzBuzz
-        
-        */
+        for (int i = 1; i <= 150; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
     }
     
-}
+
