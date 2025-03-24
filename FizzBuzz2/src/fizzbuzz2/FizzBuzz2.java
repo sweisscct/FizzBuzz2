ae34bd3645b6c4635431c41116c17b507198c13a
Sam Weiss <sweiss@cct.ie>
2025-03-24 12:38:33 +0000

    Initial FizzBuzz project

---
+++ FizzBuzz2/src/fizzbuzz2/FizzBuzz2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fizzbuzz2;
+
+/**
+ *
+ * @author Lecturer
+ */
+public class FizzBuzz2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        FizzBuzz
+        Print the numbers between 1 and 150
+        If a number is divisible by 3, print Fizz
+        If a number is divisible by 5, print Buzz
+        If a number is divisible by both, print FizzBuzz
+        
+        */
+    }
+    
+}
